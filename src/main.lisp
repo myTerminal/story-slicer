c20472ad346e3b836900011c9e376d2cc395d504
myTerminal <team.terminal@gmail.com>
2023-02-14 17:21:36 -0600

    move input arguments validation out as a separate function

--- src/main.lisp
+++ src/main.lisp
@@ -3,18 +3,24 @@
 
 (in-package :main)
 
+(defun validate-inputs (arguments)
+  "Validates whether an input file was specified."
+  (unless (car arguments)
+    (progn
+      (log-to-stdout "Please specify an input file!")
+      (uiop:quit)))
+  (unless (cadr arguments)
+    (progn
+      (log-to-stdout "Please specify an output directory!")
+      (uiop:quit))))
+
 (defun main ()
   "The main entry point to the program."
   (let ((args (uiop:command-line-arguments)))
-    (unless (car args)
-      (progn
-        (log-to-stdout "Please specify an input file!")
-        (uiop:quit)))
-    (unless (cadr args)
-      (progn
-        (log-to-stdout "Please specify an output directory!")
-        (uiop:quit)))
-    (let* ((filename (car args))
+    ;; Run all validations on input arguments
+    (validate-inputs args)
+
+    (let* ((input-filename (car args))
            (output-directory (cadr args))
            (max-length (if (caddr args)
                            (parse-integer (caddr args) :junk-allowed t)
@@ -24,7 +30,7 @@
                              0))
            (length-of-video (parse-string-to-float (get-result-from-system (concatenate 'string
                                                                                         "ffprobe -i "
-                                                                                        filename
+                                                                                        input-filename
                                                                                         " -show_entries format=duration -v quiet -of csv=\"p=0\""))))
            (number-of-slices (ceiling (/ (- length-of-video start-offset)
                                          max-length))))
@@ -33,7 +39,7 @@
                                                "ffmpeg -ss "
                                                (write-to-string (+ (* (1- i) max-length) start-offset))
                                                " -i "
-                                               filename
+                                               input-filename
                                                " -t "
                                                (write-to-string max-length)
                                                " -c copy "
